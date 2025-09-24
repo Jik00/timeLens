54ec2b6d3bc0ed87fa525e53fe5a43c5437062a5
Jiko <youstinahabib422@gmail.com>
2025-09-24 14:53:12 +0300

    splash animation changed

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -18,10 +18,6 @@ class Assets {
   /// assets/images/3.jpg
   static const String assetsImages3 = "assets/images/3.jpg";
 
-  /// Assets for assetsImages4
-  /// assets/images/4.jpg
-  static const String assetsImages4 = "assets/images/4.jpg";
-
   /// Assets for assetsImages5
   /// assets/images/5.jpg
   static const String assetsImages5 = "assets/images/5.jpg";
@@ -30,8 +26,52 @@ class Assets {
   /// assets/images/6.jpg
   static const String assetsImages6 = "assets/images/6.jpg";
 
+  /// Assets for assetsImages7
+  /// assets/images/7.jpg
+  static const String assetsImages7 = "assets/images/7.jpg";
+
+  /// Assets for assetsImages8
+  /// assets/images/8.jpg
+  static const String assetsImages8 = "assets/images/8.jpg";
+
   /// Assets for assetsImagesAppIcon
   /// assets/images/app_icon.png
   static const String assetsImagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for assetsImagesPharaoh
+  /// assets/images/pharaoh.png
+  static const String assetsImagesPharaoh = "assets/images/pharaoh.png";
+
+  /// Assets for assetsImagesSplash1
+  /// assets/images/splash1.png
+  static const String assetsImagesSplash1 = "assets/images/splash1.png";
+
+  /// Assets for assetsImagesSplash2
+  /// assets/images/splash2.png
+  static const String assetsImagesSplash2 = "assets/images/splash2.png";
+
+  /// Assets for assetsImagesSplash3
+  /// assets/images/splash3.png
+  static const String assetsImagesSplash3 = "assets/images/splash3.png";
+
+  /// Assets for assetsImagesSplash4
+  /// assets/images/splash4.png
+  static const String assetsImagesSplash4 = "assets/images/splash4.png";
+
+  /// Assets for assetsImagesSplash5
+  /// assets/images/splash5.png
+  static const String assetsImagesSplash5 = "assets/images/splash5.png";
+
+  /// Assets for assetsImagesSplash6
+  /// assets/images/splash6.png
+  static const String assetsImagesSplash6 = "assets/images/splash6.png";
+
+  /// Assets for assetsImagesSplash7
+  /// assets/images/splash7.png
+  static const String assetsImagesSplash7 = "assets/images/splash7.png";
+
+  /// Assets for assetsImagesSplash8
+  /// assets/images/splash8.png
+  static const String assetsImagesSplash8 = "assets/images/splash8.png";
 }
 
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timelens/core/utils/app_images.dart';
-import 'package:timelens/core/widgets/app_logo.dart';
-import 'package:timelens/features/splash/presentation/views/widgets/lottie_logo_pyramid.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -10,23 +10,33 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody>
-    with SingleTickerProviderStateMixin {
-//
-  bool isLogo = true;
-  late AnimationController _controller;
+class _SplashViewBodyState extends State<SplashViewBody> {
+  
+  final List<String> _icons = [
+    Assets.assetsImagesSplash1,
+    Assets.assetsImagesSplash2,
+    Assets.assetsImagesSplash3,
+    Assets.assetsImagesSplash4,
+    Assets.assetsImagesSplash6,
+    Assets.assetsImagesSplash8,
+    Assets.assetsImagesSplash7,
+  ];
+
+  int _currentIndex = 0;
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
-    initAnimation();
-    toggleLogo();
-    initNavigation();
+
+    _initTimer();
+
+    _initNavigation();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _timer?.cancel();
     super.dispose();
   }
 
@@ -34,47 +44,59 @@ class _SplashViewBodyState extends State<SplashViewBody>
   Widget build(BuildContext context) {
     return Stack(
       children: [
-//backgroung WILL BE CHANGED SOON!
+        //backgroung WILL BE CHANGED SOON!
         SizedBox.expand(
           child: Image.asset(
-            Assets.assetsImages0,
+            Assets.assetsImages7,
             fit: BoxFit.cover,
           ),
         ),
-//
+        
+        // Dark edges effect using gradient
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black.withOpacity(0.6), // top darker
+                Colors.transparent, // middle
+                Colors.black.withOpacity(0.88), // bottom darker
+              ],
+            ),
+          ),
+        ),
+
         Center(
-          child: AnimatedCrossFade(
-            crossFadeState:
-                isLogo ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-            duration: const Duration(milliseconds: 300),
-            alignment: Alignment.center,
-            firstCurve: Curves.bounceInOut,
-//
-            firstChild: LottieLogoPyramid(controller: _controller),
-            secondChild: const AppLogo(),
+          child: ClipOval(
+            child: Image.asset(
+              _icons[_currentIndex],
+              width: 140.w,
+              height: 140.h,
+              fit: BoxFit.fill,
+            ),
           ),
         ),
       ],
     );
   }
 
-  // our init logic
-  Future<Null> toggleLogo() {
-    return Future.delayed(const Duration(milliseconds: 3000), () {
-      setState(() {
-        isLogo = false;
-      });
-    });
+ //our logic
+  Future<Null> _initNavigation() {
+    return Future.delayed(const Duration(seconds: 5), () {
+    _timer?.cancel();
+
+    Navigator.pushReplacementNamed(context, 'OnboardingView');
+
+  });
   }
 
-  Future<Null> initNavigation() {
-    return Future.delayed(const Duration(milliseconds: 3700), () {
-      Navigator.pushReplacementNamed(context, 'OnboardingView');
+  Timer _initTimer() {
+    return _timer = Timer.periodic(const Duration(milliseconds: 750), (timer) {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % _icons.length;
     });
+  });
   }
 
-  AnimationController initAnimation() {
-    return _controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 800));
-  }
 }
