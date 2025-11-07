fdd4010b6d9837ad46aed061c0829e8203a12194
Jiko <youstinahabib422@gmail.com>
2025-11-08 00:51:43 +0200

    custom form text field with null validator

---
+++ lib/features/auth/presentation/views/widgets/custom_form_text_field.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:timelens/core/utils/app_colors.dart';
+import 'package:timelens/core/utils/context_extensions.dart';
+
+class CustomFormTextfield extends StatelessWidget {
+  const CustomFormTextfield(
+      {super.key,
+      required this.textInputType,
+      required this.hintText,
+      this.suffixIcon,
+      this.prefixIcon,
+      this.onSaved});
+
+  final TextInputType textInputType;
+  final String hintText;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final void Function(String?)? onSaved;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 278.w,
+      child: TextFormField(
+        onSaved: onSaved,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return context.loc.requiredField;
+          }
+          return null;
+        },
+        keyboardType: textInputType,
+        decoration: InputDecoration(
+          labelText: hintText,
+          hintStyle: TextStyle(
+            fontSize: 16.sp,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+          suffixIcon: suffixIcon,
+          prefixIcon: prefixIcon,
+          filled: true,
+          fillColor: Colors.transparent,
+          enabled: true,
+          enabledBorder: buildBorder(),
+          border: buildBorder(),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.r),
+      borderSide: const BorderSide(
+        width: 1,
+        color: AppColors.primaryColor,
+      ),
+    );
+  }
+}
