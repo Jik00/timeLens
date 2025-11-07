0f1232487b42b3a685767416ab323dfcd73bfa59
Jiko <youstinahabib422@gmail.com>
2025-11-08 00:51:02 +0200

    fix singleChildScrollView with stack and positioned

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -5,6 +5,7 @@ import 'package:timelens/core/utils/app_colors.dart';
 import 'package:timelens/core/utils/app_images.dart';
 import 'package:timelens/core/widgets/background_theme.dart';
 import 'package:timelens/features/auth/presentation/views/widgets/blur_login_card.dart';
+import 'package:timelens/features/auth/presentation/views/widgets/login_card.dart';
 import 'package:timelens/features/auth/presentation/views/widgets/login_title.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -12,34 +13,45 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        const BackgroundTheme(img: Assets.assetsImagesPyramidLogin),
-        Positioned(
-          left: 0,
-          right: 0,
-          top: 136.h,
-          child: Column(
-            children: [
-              Transform.scale(
-                scaleX: 1.1,
-                child: const LoginTitle(
-                    title: kAppTitle,
-                    colors: [
-                      AppColors.primaryColor,
-                      AppColors.primaryColor,
-                      AppColors.middleColor,
-                      Colors.white
-                    ],
-                    titleSize: 50,
-                    borderColor: AppColors.secondaryColor),
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: 1.6.sh,
+        child: Stack(
+          children: [
+            const BackgroundTheme(img: Assets.assetsImagesPyramidLogin),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 136.h,
+              child: Column(
+                children: [
+                  Transform.scale(
+                    scaleX: 1.1,
+                    child: const LoginTitle(
+                        title: kAppTitle,
+                        colors: [
+                          AppColors.primaryColor,
+                          AppColors.primaryColor,
+                          AppColors.middleColor,
+                          Colors.white
+                        ],
+                        titleSize: 50,
+                        borderColor: AppColors.secondaryColor),
+                  ),
+                  SizedBox(height: 85.h),
+                  const BlurLoginCard(),
+                ],
               ),
-              SizedBox(height: 85.h),
-              const BlurLoginCard(),
-            ],
-          ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 300.h,
+              child: const LoginCard(),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
