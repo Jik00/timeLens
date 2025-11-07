34fdfd5794e540571658757f0787306d3d147579
Jiko <youstinahabib422@gmail.com>
2025-11-07 15:12:46 +0200

    refactor images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,22 +2,6 @@
 class Assets {
   Assets._();
   
-  /// Assets for assetsImages0
-  /// assets/images/0.jpg
-  static const String assetsImages0 = "assets/images/0.jpg";
-
-  /// Assets for assetsImages1
-  /// assets/images/1.jpg
-  static const String assetsImages1 = "assets/images/1.jpg";
-
-  /// Assets for assetsImages3
-  /// assets/images/3.jpg
-  static const String assetsImages3 = "assets/images/3.jpg";
-
-  /// Assets for assetsImages6
-  /// assets/images/6.jpg
-  static const String assetsImages6 = "assets/images/6.jpg";
-
   /// Assets for assetsImagesAppIcon
   /// assets/images/app_icon.png
   static const String assetsImagesAppIcon = "assets/images/app_icon.png";
@@ -26,21 +10,9 @@ class Assets {
   /// assets/images/book_onboarding.png
   static const String assetsImagesBookOnboarding = "assets/images/book_onboarding.png";
 
-  /// Assets for assetsImagesPharaoh
-  /// assets/images/pharaoh.png
-  static const String assetsImagesPharaoh = "assets/images/pharaoh.png";
-
-  /// Assets for assetsImagesPyramidOnboarding
-  /// assets/images/pyramid_onboarding.jpg
-  static const String assetsImagesPyramidOnboarding = "assets/images/pyramid_onboarding.jpg";
-
   /// Assets for assetsImagesPyramidSplash
   /// assets/images/pyramid_splash.png
   static const String assetsImagesPyramidSplash = "assets/images/pyramid_splash.png";
-
-  /// Assets for assetsImagesPyramidsplash2
-  /// assets/images/pyramidsplash2.png
-  static const String assetsImagesPyramidsplash2 = "assets/images/pyramidsplash2.png";
 
   /// Assets for assetsImagesWallsOnboarding
   /// assets/images/walls_onboarding.png
