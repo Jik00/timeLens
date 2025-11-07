7dcb7ce6c3b398034b996aa400aab7c1aeb6db5b
Jiko <youstinahabib422@gmail.com>
2025-11-08 00:49:16 +0200

    blur card widget

--- lib/features/auth/presentation/views/widgets/blur_login_card.dart
+++ lib/features/auth/presentation/views/widgets/blur_login_card.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timelens/core/utils/app_colors.dart';
@@ -13,20 +11,20 @@ class BlurLoginCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-        child: Container(
-          width: 340.w,
-          height: 355.h,
-          decoration: BoxDecoration(
-            color: AppColors.secondaryColor.withOpacity(0.18),
-            borderRadius: BorderRadius.circular(34),
-            border: Border.all(
-              color: AppColors.primaryColor.withOpacity(0.3),
-              width: 1.5,
-            ),
+      // child: BackdropFilter(
+      // filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+      child: Container(
+        width: 340.w,
+        height: 355.h,
+        decoration: BoxDecoration(
+          color: AppColors.secondaryColor.withOpacity(0.18),
+          borderRadius: BorderRadius.circular(34),
+          border: Border.all(
+            color: AppColors.primaryColor.withOpacity(0.6),
+            width: 1.5,
           ),
         ),
+        // ),
       ),
     );
   }
