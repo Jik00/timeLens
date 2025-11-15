a0346117248e7f1b20c6e6b219dfe02189b44860
Jiko <youstinahabib422@gmail.com>
2025-11-15 20:14:00 +0200

    firestore -4- [implement addData method in FirestoreService]

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timelens/core/services/database_service.dart';
 
 class FirestoreService implements DatabaseService{
+
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
   @override
-  Future<void> addData({required String path, required Map<String, dynamic> data}) {
+  Future<void> addData({required String path, required Map<String, dynamic> data}) async{
     
-    throw UnimplementedError();
+    await _firestore.collection(path).add(data);
+    
   }
 }
