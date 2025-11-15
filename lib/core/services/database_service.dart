11cd435b8da3b80f72d68e548f8bcba368269798
Jiko <youstinahabib422@gmail.com>
2025-11-15 19:31:53 +0200

    firestore -3- [create addData method, implemt it]

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,1 +1,4 @@
-abstract class DatabaseService {}
+abstract class DatabaseService {
+  Future<void> addData(
+      {required String path, required Map<String, dynamic> data});
+}
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,3 +1,9 @@
 import 'package:timelens/core/services/database_service.dart';
 
-class FirestoreService implements DatabaseService{}
+class FirestoreService implements DatabaseService{
+  @override
+  Future<void> addData({required String path, required Map<String, dynamic> data}) {
+    
+    throw UnimplementedError();
+  }
+}
