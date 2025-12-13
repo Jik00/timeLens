7674843f3cb0081ad06dac7cc02e54e4cd6d6c62
Jiko <youstinahabib422@gmail.com>
2025-12-14 00:32:52 +0200

    figures-2- add navigation from home , add carousel slider + images needed

--- lib/features/home/presentation/views/widgets/era_container.dart
+++ lib/features/home/presentation/views/widgets/era_container.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timelens/core/utils/app_colors.dart';
 import 'package:timelens/core/widgets/stroke_text_cinzel.dart';
+import 'package:timelens/features/figures/presentation/views/figure_view.dart';
 import 'package:timelens/features/home/domain/entities/era_entity.dart';
 
 class EraContainer extends StatelessWidget {
@@ -12,58 +13,57 @@ class EraContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //width: 320.w,
-      decoration: BoxDecoration(
-        color: AppColors.secondaryColor.withOpacity(0.3),
-        borderRadius: BorderRadius.circular(34),
-        border: Border.all(
-          color: AppColors.primaryColor,
-          width: 1.5,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, FigureView.routeName);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.secondaryColor.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(34),
+          border: Border.all(
+            color: AppColors.primaryColor,
+            width: 1.5,
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 16.h,
-          ),
-          StrokeTextCizel(
-              title: eraEntity.eraName,
-              colors: const [Colors.white, Colors.white],
-              titleSize: 16,
-              borderColor: AppColors.primaryColor),
-          SizedBox(
-            height: 8.h,
-          ),
-          Container(
-            width: 300.w,
-            height: 297.h,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(34),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 16.h,
             ),
-            child: CachedNetworkImage(
+            StrokeTextCizel(
+                title: eraEntity.eraName,
+                colors: const [Colors.white, Colors.white],
+                titleSize: 16,
+                borderColor: AppColors.primaryColor),
+            SizedBox(
+              height: 8.h,
+            ),
+            CachedNetworkImage(
               imageUrl: eraEntity.imageUrl,
-              fit: BoxFit.cover,
+              width: 292.w,
+              height: 260.h,
+              fit: BoxFit.contain,
               errorWidget: (context, url, error) => const Icon(Icons.error),
               placeholder: (context, url) =>
                   const Center(child: CircularProgressIndicator()),
             ),
-          ),
-          SizedBox(
-            height: 8.h,
-          ),
-          Transform.translate(
-            offset: const Offset(80, 0),
-            child: StrokeTextCizel(
-                title: eraEntity.eraPeriod,
-                colors: const [Colors.white, Colors.white],
-                titleSize: 14,
-                borderColor: AppColors.primaryColor),
-          ),
-          SizedBox(
-            height: 10.h,
-          )
-        ],
+            SizedBox(
+              height: 8.h,
+            ),
+            Transform.translate(
+              offset: const Offset(80, 0),
+              child: StrokeTextCizel(
+                  title: eraEntity.eraPeriod,
+                  colors: const [Colors.white, Colors.white],
+                  titleSize: 14,
+                  borderColor: AppColors.primaryColor),
+            ),
+            SizedBox(
+              height: 10.h,
+            )
+          ],
+        ),
       ),
     );
   }
