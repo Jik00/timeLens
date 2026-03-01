154da6aa95c371ec67e902a7cb41560faa71518d
Jiko <youstinahabib422@gmail.com>
2026-03-01 12:26:58 +0200

    home frature - make cubit extend equatable

--- lib/features/home/presentation/cubit/cubit/get_eras_list_state.dart
+++ lib/features/home/presentation/cubit/cubit/get_eras_list_state.dart
@@ -1,7 +1,11 @@
 part of 'get_eras_list_cubit.dart';
 
 @immutable
-sealed class GetErasListState {}
+sealed class GetErasListState extends Equatable {
+
+  @override
+  List<Object?> get props => [];
+}
 
 final class GetErasListInitial extends GetErasListState {}
 
@@ -11,9 +15,15 @@ final class GetErasListSuccess extends GetErasListState {
   final List<EraEntity> eras;
 
   GetErasListSuccess({required this.eras});
+
+  @override
+  List<Object?> get props => [eras];
 }
 
 final class GetErasListFailure extends GetErasListState {
   final String message;
   GetErasListFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
