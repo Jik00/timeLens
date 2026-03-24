173dccddc5b2740607e8a30e9dd5b8f55db583f2
Jiko <youstinahabib422@gmail.com>
2026-03-24 20:25:49 +0200

    navigation -2- nested nav - global nav keys -> for each nav bar tab -> main screens - inside indexed stack, not view, but navigator builder with index, kol index to tab - nav builder handel nested navigation -> same as main function that have all routes - it takes global nav key [index], de el fekra

--- lib/features/home/presentation/views/widgets/main_layout_body.dart
+++ lib/features/home/presentation/views/widgets/main_layout_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timelens/core/helper_functions/ongenerate_routes.dart';
 import 'package:timelens/features/chatbot/presentation/views/chatbot_view.dart';
 import 'package:timelens/features/home/presentation/views/home_view.dart';
 import 'package:timelens/features/weather/presentation/views/weather_view.dart';
@@ -12,16 +13,32 @@ class MainLayoutBody extends StatefulWidget {
 }
 
 class _MainLayoutBodyState extends State<MainLayoutBody> {
+  // navigation keys -> nested navigation stack
+  final List<GlobalKey<NavigatorState>> _navigatorKeys = [
+    GlobalKey<NavigatorState>(), // HomeView
+    GlobalKey<NavigatorState>(), // ChatbotView
+    GlobalKey<NavigatorState>(), // For WeatherView
+  ];
+
   @override
   Widget build(BuildContext context) {
     return IndexedStack(
       index: widget.currentViewIndex,
-      children: const [
-        HomeView(),
-        ChatbotView(),
-        WeatherView(),
-        //WebView(),
+      children: [
+        // Wrapped Navigator fun to handle inside navigation stack
+        _buildNavigator(0, HomeView.routeName),
+        _buildNavigator(1, ChatbotView.routeName),
+        _buildNavigator(2, WeatherView.routeName),
       ],
     );
   }
+
+  Widget _buildNavigator(int index, String initialRoute) {
+    return Navigator(
+      key: _navigatorKeys[index],
+      initialRoute: initialRoute,
+      onGenerateRoute: onGenerateRoutes,
+    );
+  }
+  
 }
